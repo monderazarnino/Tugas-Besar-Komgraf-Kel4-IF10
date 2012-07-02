ca770ca60b4334c41f8a4b1cb54c90b9cb8acbcb
Refi Meisadri <rmghif@gmail.com>
2012-07-03 04:58:48 +0700

    edit jadi

--- main.cpp
+++ main.cpp
@@ -20,6 +20,12 @@
 #define KANAN3 'f'
 #define DEPAN3 'g'
 #define BELAKANG3 'h'
+#define ATAS4'z'
+#define BAWAH4 'x'
+#define KIRI4 'c'
+#define KANAN4 'v'
+#define DEPAN4 'b'
+#define BELAKANG4 'n'
 
 #define CHANGEMOVE '\t' //tab
 #define MAKEMOVE 13	//Enter
@@ -111,8 +117,9 @@ public:
 			double y=yi*1.1;
 			double z=zi*1.1;
 			
-			kubus[n].face[0].r=.8;
+			kubus[n].face[0].r=.6;
 			kubus[n].face[0].g=kubus[n].face[0].b=0;
+			
 			kubus[n].face[0].v[0].x=x-.5;		//Bawah
 			kubus[n].face[0].v[0].y=y-.5;
 			kubus[n].face[0].v[0].z=z-.5;
@@ -129,9 +136,7 @@ public:
 			kubus[n].face[0].v[3].y=y+.5;
 			kubus[n].face[0].v[3].z=z-.5;
 
-			kubus[n].face[0].v[4].x=x-.5;
-			kubus[n].face[0].v[4].y=y+.5;
-			kubus[n].face[0].v[4].z=z-.5;
+			
 
 			kubus[n].face[1].r=.10;
 			kubus[n].face[1].g=.10;
@@ -152,11 +157,10 @@ public:
 			kubus[n].face[1].v[3].y=y+.5;
 			kubus[n].face[1].v[3].z=z+.5;
 
-			kubus[n].face[1].v[4].x=x-.5;		
-			kubus[n].face[1].v[4].y=y+.5;
-			kubus[n].face[1].v[4].z=z+.5;			
+				
 			
 			kubus[n].face[2].r=kubus[n].face[2].b=kubus[n].face[2].g=.9;
+			
 			kubus[n].face[2].v[0].x=x-.5;		//Kiri
 			kubus[n].face[2].v[0].y=y-.5;
 			kubus[n].face[2].v[0].z=z-.5;
@@ -173,9 +177,7 @@ public:
 			kubus[n].face[2].v[3].y=y+.5;
 			kubus[n].face[2].v[3].z=z-.5;
 
-			kubus[n].face[2].v[4].x=x-.5;
-			kubus[n].face[2].v[4].y=y+.5;
-			kubus[n].face[2].v[4].z=z-.5;			
+						
 
 			kubus[n].face[3].r=kubus[n].face[3].g=.95;
 			kubus[n].face[3].b=0;
@@ -195,9 +197,7 @@ public:
 			kubus[n].face[3].v[3].y=y+.5;
 			kubus[n].face[3].v[3].z=z-.5;
 
-			kubus[n].face[3].v[4].x=x+.5;
-			kubus[n].face[3].v[4].y=y+.5;
-			kubus[n].face[3].v[4].z=z-.5;			
+					
 			
 			kubus[n].face[4].b=1;
 			kubus[n].face[4].r=kubus[n].face[4].g=0;
@@ -217,12 +217,11 @@ public:
 			kubus[n].face[4].v[3].y=y-.5;
 			kubus[n].face[4].v[3].z=z-.5;
 
-			kubus[n].face[4].v[4].x=x+.5;
-			kubus[n].face[4].v[4].y=y-.5;
-			kubus[n].face[4].v[4].z=z-.5;			
+						
 			
 			kubus[n].face[5].g=.6;
 			kubus[n].face[5].b=kubus[n].face[5].r=0;
+			
 			kubus[n].face[5].v[0].x=x-.5;	//Belakang
 			kubus[n].face[5].v[0].y=y+.5;
 			kubus[n].face[5].v[0].z=z-.5;
@@ -239,16 +238,8 @@ public:
 			kubus[n].face[5].v[3].y=y+.5;
 			kubus[n].face[5].v[3].z=z-.5;
 
-			kubus[n].face[5].v[4].x=x+.5;
-			kubus[n].face[5].v[4].y=y+.5;
-			kubus[n].face[5].v[4].z=z-.5;			
+		
 			
-			for(int f=0;f<7;++f)
-				for(int v=0;v<5;++v)
-					if(fabs(kubus[n].face[f].v[v].x)<1
-						&& fabs(kubus[n].face[f].v[v].y)<1
-						&& fabs(kubus[n].face[f].v[v].z)<1) 
-						kubus[n].face[f].r=kubus[n].face[f].g=kubus[n].face[f].b=0;
 		}
 	}
 	void Draw()
@@ -261,8 +252,8 @@ public:
 	void MakeMove(char k,bool fast)
 	{
 		Matrix m;
-		for(int x=0;x<5;++x)
-			for(int y=0;y<5;++y)
+		for(int x=0;x<4;++x)
+			for(int y=0;y<4;++y)
 				m.l[x][y]=0;
 	
 		double ang=fast?3.14159265/10.0:3.14159265/50.0;
@@ -272,10 +263,12 @@ public:
 		case(KIRI):
 		case(KIRI2):
 		case(KIRI3):
-			m.l[0][0]=1;
+        case(KIRI4):            
+        	m.l[0][0]=1;
 			m.l[1][1]=cos(ang);
 			m.l[1][2]=sin(ang);
 			m.l[2][1]=-sin(ang);
+			m.l[2][2]=cos(ang);
 			m.l[2][2]=cos(ang);
 			break;
 		case(KANAN):
@@ -299,6 +292,7 @@ public:
 		case(BAWAH):
 		case(BAWAH2):
 		case(BAWAH3):
+  		case(BAWAH4):
 			m.l[2][2]=1;
 			m.l[0][0]=cos(ang);
 			m.l[0][1]=sin(ang);
@@ -308,6 +302,7 @@ public:
 		case(DEPAN):
 		case(DEPAN2):
 		case(DEPAN3):
+        case(DEPAN4):
 			m.l[1][1]=1;
 			m.l[0][0]=cos(ang);
 			m.l[0][2]=sin(ang);
@@ -331,7 +326,7 @@ public:
 			for(int n=0;n<64;++n)
 			switch(k)
 			{
-			case(KIRI):
+            case(KIRI):
 			case(KANAN3):
 				if(kubus[n].Centre().x<-1) kubus[n].MultiplyBy(m);
 				break;
@@ -379,9 +374,9 @@ int main(int argc,char** argv)
 {
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(600,600);
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(300,100);
 	glutCreateWindow("Rubik's Kubus");
-
 	glutReshapeFunc(UkuranLayar);
 	glutDisplayFunc(KotakMoving);
 	glutKeyboardFunc(PutarRubik);
@@ -396,9 +391,9 @@ int main(int argc,char** argv)
 
 void UkuranLayar(int x,int y)
 {
-	if(x!=600 || y!=600) //untuk mengunci ukuran window di 500x500
+	if(x!=500 || y!=500) //untuk mengunci ukuran window di 500x500
 	{
-		glutReshapeWindow(600,600);
+		glutReshapeWindow(500,500);
 		return;
 	}
 	
@@ -412,7 +407,7 @@ void UkuranLayar(int x,int y)
 
 void KotakMoving() //fungsi membuat tanda kotak pergerakan 
 {
-	glClearColor(6,2,.4,.5);
+	glClearColor(0,2,.4,.5);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	glLoadIdentity();	
@@ -420,22 +415,22 @@ void KotakMoving() //fungsi membuat tanda kotak pergerakan
 
 
 	glPushMatrix();
-		glLineWidth(5); //tebal garis kotak moving
+		glLineWidth(4); //tebal garis kotak moving
 		glColor3d(0,0,0); //warna garisnya
 		if(GerakanKotak<4) //kondisi pergerakannya
 		{
 			glScaled(1.1,4.7,4.7);
 			glTranslated(GerakanKotak-1,0,0);
 		}
-		if(GerakanKotak>=4 && GerakanKotak<7)
+		if(GerakanKotak>=4 && GerakanKotak<8)
 		{
 			glScaled(4.7,1.1,4.7);
-			glTranslated(0,GerakanKotak-4,0);	
+			glTranslated(0,GerakanKotak-5,0);	
 		}
-		if(GerakanKotak>=7)
+		if(GerakanKotak>=8)
 		{
 			glScaled(4.7,4.7,1.1);
-			glTranslated(0,0,GerakanKotak-7);
+			glTranslated(0,0,GerakanKotak-9);
 		}
 		glutWireCube(1);
 	glPopMatrix();
@@ -448,7 +443,7 @@ void PutarRubik(unsigned char k,int x,int y) //fungsi untuk memutar warna-warna 
 	if(k==CHANGEMOVE)
 	{
 		++GerakanKotak;
-		GerakanKotak%=16;
+		GerakanKotak%=12;
 		glutPostRedisplay();
 		return;
 	}
@@ -457,12 +452,18 @@ void PutarRubik(unsigned char k,int x,int y) //fungsi untuk memutar warna-warna 
 		if(GerakanKotak==0) RKubus.MakeMove(KIRI,false);
 		if(GerakanKotak==1) RKubus.MakeMove(KIRI2,false);
 		if(GerakanKotak==2) RKubus.MakeMove(KIRI3,false);
-		if(GerakanKotak==3) RKubus.MakeMove(DEPAN,false);
-		if(GerakanKotak==4) RKubus.MakeMove(DEPAN2,false);
-		if(GerakanKotak==5) RKubus.MakeMove(DEPAN3,false);
-		if(GerakanKotak==6) RKubus.MakeMove(BAWAH,false);
-		if(GerakanKotak==7) RKubus.MakeMove(BAWAH2,false);
-		if(GerakanKotak==8) RKubus.MakeMove(BAWAH3,false);
+		if(GerakanKotak==3) RKubus.MakeMove(KIRI3,false);
+		
+		
+		if(GerakanKotak==4) RKubus.MakeMove(DEPAN,false);
+		if(GerakanKotak==5) RKubus.MakeMove(DEPAN2,false);
+		if(GerakanKotak==6) RKubus.MakeMove(DEPAN3,false);
+		if(GerakanKotak==7) RKubus.MakeMove(DEPAN3,false);
+		
+		if(GerakanKotak==8) RKubus.MakeMove(BAWAH,false);
+		if(GerakanKotak==9) RKubus.MakeMove(BAWAH2,false);
+		if(GerakanKotak==10) RKubus.MakeMove(BAWAH3,false);
+		if(GerakanKotak==11) RKubus.MakeMove(BAWAH3,false);
 		return;
 	}
 }
